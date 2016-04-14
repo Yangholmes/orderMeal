12bb06697d0dd21ab4367daa54c67a080b31ed5a
unknown <>
2016-04-14 23:02:00 +0800

    decoration ordermeal.

--- manage/terminal/mysql-query.php
+++ manage/terminal/mysql-query.php
@@ -4,7 +4,7 @@ require_once('yangLib.php');
 
 $json = $_POST['data'];
 
-$data = json_decode($json);//Obj
+$data = json_decode($json);//String -> Object
 
 $cmd = $data->content->cmd;//获取指令
 
@@ -13,7 +13,7 @@ $pattern = "/^mysql -h|^mysql -u/";//mysql登陆表达式
 if( preg_match( $pattern, $cmd) ){//判断是否需要登陆mysql
 	$patterns = array( "/-h [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|-h [a-zA-Z0-9\-\.]+/", 
 					  "/-u [a-zA-Z0-9_\-\.]+/", 
-					  "/-p .+/");
+					  "/-p .*/");
 	$matches = preg_match_patterns($patterns, $cmd);
 
 	@ $mysqli = new mysqli("$matches[0]", "$matches[1]", "$matches[2]");
@@ -21,7 +21,7 @@ if( preg_match( $pattern, $cmd) ){//判断是否需要登陆mysql
 	if($mysqli->connect_errno){
 		$content['connect_errno'] = $mysqli->connect_errno;
 		$content['connect_error'] = $mysqli->connect_error;
-		$content['host_info'] = $mysqli->host_info;	
+		$content['host_info'] = $mysqli->host_info;
 		$connectMsg = new mysqlMsg( 0, $content, 1 );
 	}
 	else{
@@ -29,12 +29,14 @@ if( preg_match( $pattern, $cmd) ){//判断是否需要登陆mysql
 		$content['usr_info'] =  "$matches[1]:$matches[2]@$matches[0]";
 		$connectMsg = new mysqlMsg( 1, $content, 1 );
 	}
-	// $result = $mysqli->query ( "use ordermeal" );
-	// $result = $mysqli->query ( "select * from personnel" );
-	// echo json_encode($result);
+	
 	echo json_encode($connectMsg);
 }
 
+
+/**
+ * 消息类
+ */
 class mysqlMsg
 {
 	public $query = 0;
