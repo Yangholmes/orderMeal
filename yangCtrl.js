f0ad20980eb0aeec0c0ae29055cfbf1fea428d8e
Yangholmes Liang <Yangholmes Liang>
2016-05-12 17:26:58 +0800

    almost finish _createStyle().

--- yangCtrl.js
+++ yangCtrl.js
@@ -2,45 +2,79 @@ function yangImgInput( name, message, img ){
 	this.name = name;
 	this.message = message;
 	this.img = img;
-
-/*	var allCongener = document.getElementsByClassName("yang-img-input"); //getElementsByClassName可能会无此方法，最好做渐进增强
-	if(!allCongener)
-		this.id = 'yang-img-input-' + '0';
-	else
-		this.id = 'yang-img-input-' + (allCongener.length);*/
 }
 
 yangImgInput.prototype = {
 	ctrlName: 'yang-img-input',
+	id: '',
+	selfDom: '',
 
 	width: 'auto',
 	height: 'auto';
-	style: '',
-	maskStyle: '',
 	img: '',
 	message: 'click here!',
 	name: 'imgInput',
 
 	html: 
 			'<div id="{yang-img-input-id}" class="yang-img-input">' + 
-				'<div id="{yang-img-input-id}-touch-layout">' + 
-					'<div id="{yang-img-input-id}-mask"></div>' + 
-					'<div id="{yang-img-input-id}-message">' + this.message + '</div>' + 
-					'<img id="{yang-img-input-id}-preview" src="" />' + 
+				'<div id="{yang-img-input-id}-touch-layout" class="yang-img-input-touch-layout">' + 
+					'<div id="{yang-img-input-id}-mask" class="yang-img-input-mask"></div>' + 
+					'<div id="{yang-img-input-id}-message" class="yang-img-input-message">' + this.message + '</div>' + 
+					'<img id="{yang-img-input-id}-preview" class="yang-img-input-preview" src="" />' + 
 				'</div>' + 
-				'<input type="file" id="{yang-img-input-id}-choose" name="' + this.name + '" accept="image/*" />' + 
+				'<input type="file" id="{yang-img-input-id}-choose" class="yang-img-input-choose" name="' + this.name + '" accept="image/*" />' + 
 			'</div>',
-	id: '',
+	style: {
+				'yang-img-input': 'margin: 0; width: auto; height: aoto;',
+				'yang-img-input-touch-layout': 'margin: 0;', 
+				'yang-img-input-mask': 'position: absolute;', 
+				'yang-img-input-message': 'color: rgba(220, 220, 220, 0); position: absolute; text-align: center;', 
+				'yang-img-input-preview': 'width:100%; height: 100%;', 
+				'yang-img-input-choose': 'position: absolute; width: 0; visibility: hidden;'
+			},
+	maskStyle: 'background-color: rgba(0,0,0,0);',
 
+	rendered: false,
+
+	_instanceId: function(){	
+		var allCongener = document.getElementsByClassName("yang-img-input"); //getElementsByClassName可能会无此方法，最好做渐进增强
+		if(!allCongener)
+			this.id = 'yang-img-input-' + '0';
+		else
+			this.id = 'yang-img-input-' + (allCongener.length);
+	},
 	_createHTML: function(){
 		var that = this;
 		if( !that.name )
 			return false;
-		instanceId(that.html); //实例化id
+		that._instanceId(); //generate an id
+		that.html = _replaceId(that.html); //replaceId
 		return that.html;
 	},
 	_createStyle: function(){
-		var that = this;
+		var that = this, 
+		generalStyle = this.style, 
+		maskStyle = this.maskStyle, 
+		style = '',
+		styleElt = document.createElement('style');
+		for( selector in generalStyle ){
+			style += '.' + selector + '{' + generalStyle[selector] +'}';
+		}
+		style +=  '#' + that.id + '-mask' + '{' + this.maskStyle + '}';
+
+		styleElt.innerHTML = style;
+		document.getElementsByTagName('head')[0].appendChild(styleElt);
+	},
+	_replaceId: function(text){
+		var instanceIdRegExp = '/' + this.id + '/g';
+		return text.replace(instanceIdRegExp, '{yang-img-input-id}');
+	},
+
+	render: function(e){
+		if(!rendered){
+			_createStyle();
+			_createHTML();
+		}
 	},
 
 	setName: function(name){
@@ -66,7 +100,7 @@ yangImgInput.prototype = {
 		this.setImg();
 	},
 
-	insert: function(){},
-	insertBefore: function(){},
-	insertAfter: function(){},
+	insert: function(e){},
+	insertBefore: function(e){},
+	insertAfter: function(e){},
 }
