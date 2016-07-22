86ad88dba927b741db9cb3203158abf9d7ec15f1
Yangholmes Liang <Yangholmes Liang>
2016-07-22 10:02:49 +0800

    new UI.

--- manage/index.js
+++ manage/index.js
@@ -1,3 +1,11 @@
+window.onload = init();
+/**
+ * 
+ */
+function init(){
+	
+}
+
 /**
  * 消息类
  * [Object]
@@ -61,127 +69,3 @@ function postData(url, data, callback) {
 							 "application/x-www-form-urlencoded");
 	request.send(encodeFormData(data));			// Send form-encoded data
 }
-
-function init(){
-	//注册图片上传按键事件
-	var chooseFile = document.getElementById('choose-file');
-	chooseFile.onchange = function(){showPreview(chooseFile)};
-	//注册鼠标进入、移出预览容器事件
-	var menuPreviewTouch = document.getElementById('menu-preview-touch');
-	menuPreviewTouch.onmouseover = function(){onMouseOver(menuPreviewTouch)};
-	menuPreviewTouch.onmouseout = function(){onMouseOut(menuPreviewTouch)};
-	//注册图片预览控件点击事件
-	// var menuPreview = document.getElementById('menu-preview');
-	// menuPreview.onclick = function(){onFireFileinput(chooseFile)};
-	menuPreviewTouch.onclick = function(){onFireFileinput(chooseFile)};
-	//注册统计按键事件
-	var countButton = document.getElementById('count-button');
-	countButton.onclick = function(){onClick(countButton)};
-	//注册未点餐按键事件
-	var mostusedButton = document.getElementById('mostused-button');
-	mostusedButton.onclick = function(){onMostusedClick(mostusedButton)};
-	//注册提交菜单按键事件
-	var menuForm = document.getElementById('menu-form');
-	menuForm.onsubmit = function(){return onSubmit(menuForm)};
-}
-function onFireFileinput(handle){
-	// var event = document.createEvent('MouseEvents');
-	// event.initMouseEvent('click');// Deprecated
-	// handle.dispatchEvent(event);
-	handle.click();
-}
-function onMouseOver(handle){
-	var mask = handle.getElementsByTagName('div')[0];
-	var message = handle.getElementsByTagName('div')[1];
-	var img = handle.getElementsByTagName('img')[0];
-	//获取菜单图片的大小
-	var imgHeight = img.offsetHeight;
-	var imgWidth = img.offsetWidth;
-	//设置mask的大小
-	mask.style.height = imgHeight + 'px';
-	mask.style.width = imgWidth + 'px';
-	//设置message的位置
-	var messageMarginTop = (imgHeight - message.offsetHeight)/2;
-	var messageMarginLeft = (imgWidth - message.offsetWidth)/2;
-	message.style.marginTop = messageMarginTop + 'px';
-	message.style.marginLeft = messageMarginLeft + 'px';
-}
-function onMouseOut(handle){
-	
-}
-function showPreview(handle){
-	if( !handle ){
-		alert("这张菜单是假的，\n请检查一下重发！");
-		return false;
-	}
-	var img = document.getElementById('menu-preview');
-	var file = handle.files[0];
-	if( !/image\/png/.test(file.type) ){
-		alert("请选择png格式图片~");
-		console.log( handle.files[0] );
-		// handle.outerHTML = handle.outerHTML.replace(/(value=\").+\"/i, "$1\"");
-		handle.value = null;
-		console.log( handle.files[0] );
-		return false;
-	}
-	if(window.FileReader && file) {
-		var reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onloadend = function(e){
-			img.src = e.target.result;
-		}
-	}
-}
-function onClick(handle){
-	if(!handle){
-		return false;
-	}
-	var countQuery = new queryMsg(1, "count", 1);
-	postData("count.php", countQuery, function(request){
-		var response = JSON.parse(request.responseText);
-		insertTable(response);
-	});
-}
-function onMostusedClick(handle){
-	if(!handle){
-		return false;
-	}
-	var countQuery = new queryMsg(1, "mostused", 1);
-	postData("count.php", countQuery, function(request){
-		var response = JSON.parse(request.responseText);
-		console.log(response);
-	});
-}
-function onSubmit(form){
-	if( form.menu.value == null || form.menu.value == "" ){
-		//alert("请上传菜单！");
-		//return false;
-	}
-	else
-		return true;
-}
-function insertTable(data){
-	var countTd = document.getElementById('count-td');
-	var detailsTb = document.getElementById('details-tb');
-	var details = data.content.details;
-	
-	countTd.innerHTML = "<td>"+ data.content.count_total +"</td>" + 
-						"<td>"+ data.content.count_A +"</td>" + 
-						"<td>"+ data.content.count_B +"</td>" + 
-						"<td>"+ data.content.count_C +"</td>" + 
-						"<td>"+ data.content.count_D +"</td>" ;
-	
-	var eachRow = "";
-	for(var i=0;i<details.length;i++){
-		eachRow = eachRow + "<tr>" +
-					"<td>" + details[i].name + "</td>" +
-					"<td>" + details[i].meal + "</td>" +
-					"</tr>";
-	}
-	detailsTb.innerHTML = eachRow;
-}
-function insertMostusedTable(){
-	
-}
-
-window.onload = init();
