21c5a8fe1f059e5184b524cf6a6ff3ced3b51fae
Yangholmes Liang <Yangholmes Liang>
2016-08-14 23:54:44 +0800

    new server begin~

--- manage/HR/index.js
+++ manage/HR/index.js
@@ -2,10 +2,9 @@ $('#personnel').w2grid({
     name   : 'personnelGrid', 
     style: 'font-size: 1.5em',
     url  : {
-        // get    : '../HR/server/records.json',
-        get:  '../HR/server/records.php',
-        remove : '../HR/server/remove.php',
-        save   : '',
+        get    : '../HR/server/personnelSearch.php',
+        // get:  '../HR/server/records.php',
+        // remove : '../HR/server/remove.php',
     },
     show : {
         toolbar : true, toolbarAdd: true, toolbarDelete: true, toolbarSave: true,
@@ -13,19 +12,18 @@ $('#personnel').w2grid({
     toolbar: {
         items: [
             { type: 'break' },
-            { type: 'button', id: 'mybutton', caption: '点我试试？', onClick: function(){console.log('clicked');}, style: 'border: 1px solid red' }
+            { type: 'button', id: 'mybutton', caption: '点我试试？', }
         ],
         onClick: function (target, data) {
             console.log(target);
         }
     },
     columns: [
-        // { field: 'personnelId', caption: '编号', size: '30%' },
         { field: 'name', caption: '姓名', size: '20%', editable: {type: 'text'} },
         { field: 'engName', caption: '英格力士名', size: '20%', editable: {type: 'text'} },
         { field: 'sex', caption: '性别', size: '20%', render: function(record){return ['女', '男'][record.sex]; }, editable: {type: 'combo', items: ['0', '1'], openOnFocus: true} },
         { field: 'age', caption: '年龄', size: '20%', editable: {type: 'text'} },
-        { field: 'department', caption: '部门', size: '20%', editable: {type: 'combo', items: ['1', '2', '3'], openOnFocus: true} },
+        { field: 'department', caption: '部门', size: '20%', render: function(record){return ['未分类', 'R&D'][record.department]; }, editable: {type: 'combo', items: ['0', '1', '2'], openOnFocus: true} },
     ],
     onChange: function(target, e) {
         e.onComplete = function () {
@@ -36,7 +34,6 @@ $('#personnel').w2grid({
         this.add({recid: this.records.length+1});
         // $('#grid_' + target +  '_edit_'+ (this.records.length) + '_1').focus();
         var td = $('#grid_' + target +  '_rec_'+ (this.records.length) + '> td:eq(0)');
-        console.log(td);
         td.trigger('click');
     },
     // records: [
