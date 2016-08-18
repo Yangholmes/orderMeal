e72ca3fed0625ab1787e418009951a6ad69c8c47
Yangholmes <yangholmes@126.com>
2016-08-18 17:17:04 +0800

    finish yang-mysql-lib step 1.

--- manage/hr/server/yang-mysql-class.php
+++ manage/hr/server/yang-mysql-class.php
@@ -57,14 +57,20 @@ class yangMysql{
 	 */
 	public function query($query){
 		$this->query = $query;
-		$this->result = $this->connection->query($this->query);
-		if(!$this->result){
+		echo "query is ## "; echo json_encode($this->query); echo "\n";
+		$result = $this->connection->query($this->query);
+		if(!$result){ //Returns FALSE on failure.
 			$this->errorHandle();
 			return false;
 		}
-		$this->row = $this->result->num_rows;
-		$this->result = $this->result->fetch_all(MYSQLI_ASSOC); //PHP 5 >= 5.3.0, PHP 7, MYSQL_ASSOC means return an association array
-		return $this->result;
+		if(!is_bool($result)){ //For successful SELECT, SHOW, DESCRIBE or EXPLAIN queries mysqli_query() will return a mysqli_result object.
+			$this->row = $result->num_rows;
+			$this->result = $result->fetch_all(MYSQLI_ASSOC); //PHP 5 >= 5.3.0, PHP 7, MYSQL_ASSOC means return an association array
+			echo "result is ## "; echo json_encode($this->result); echo "\n";
+			return $this->result;
+		}
+		else //For other successful queries mysqli_query() will return TRUE.
+			return true;
 	}
 
 	/**
@@ -90,7 +96,6 @@ class yangMysql{
 	public function getCharset(){
 		$queryCharset = "SHOW VARIABLES LIKE 'character_set_connection'";
 		$this->charset = $this->query($queryCharset)[0]["Value"];
-		echo $this->charset;
 	}
 
 	/**
@@ -103,6 +108,8 @@ class yangMysql{
 			return flase;
 		}
 		$this->db_database = $database;
+		echo json_encode($this->db_database);
+		return $this->db_database;
 	}
 
 	/**
@@ -111,7 +118,10 @@ class yangMysql{
 	public function showTables(){
 		$query = "show tables";
 		$tables = $this->query($query);
-		echo json_encode($tables);
+		if($this->connection->connect_error){
+			$this->errorHandle();
+			return flase;
+		}
 		return $tables; //[{}]
 	}
 	/**
@@ -123,8 +133,93 @@ class yangMysql{
 	}
 
 	/**
-	 * basic single table method
+	 * clear all date from $table
 	 */
+	public function truncateTable($table){
+		//remind: here must have some methods to check the table
+		$query = "TRUNCATE $table";
+		$this->query($query);
+	}
+
+	/**
+	 * decorate value
+	 */
+	private function _decorate(&$array){
+		while(list($name, $value) = each($array)){
+			if($value==null)
+				$array[$name] = "null";
+			else
+				$array[$name] = "'$value'";
+		}
+		reset($array); //reset index of the $array
+	}
+
+	/**
+	 * basic method
+	 * select from a single table
+	 * $select_col is an array [name]
+	 * $condition is a string
+	 * $order is an array [col_name, ASC | DESC]
+	 * $limite is an array [offset, row_count]
+	 */
+	public function simpleSelect($select_col, $condition, $order, $limit){
+		if($select_col){
+			$this->_decorate($select_col);
+			$select_expr = implode(",", $select_col);
+		}
+		else
+			$select_expr = "*"; //all columns
+		$where_condition = $condition ? "WHERE $condition" : "";
+		$orderby = $order ? "ORDER BY $order[0] $order[1]" : "";
+		$limitby = $limit ? "LIMIT $limit[0] $limit[1]" : "";
+		$query = "SELECT $select_expr FROM $this->db_table $where_condition $orderby $limitby";
+		$this->query($query);
+	}
+
+	/**
+	 * basic method
+	 * insert one record
+	 * $record is an associate array [name => value]
+	 */
+	public function insert($record){
+		if(!is_array($record)) return false;
+		$this->_decorate($record); //decorate the value. add "".
+		$name = implode(",", array_keys($record)); //implode() join()  Join array elements with a string
+		$value = implode(",", $record); //
+		$query = "INSERT INTO $this->db_table($name) VALUES($value)";
+		$this->query($query);
+	}
+
+	/**
+	 * basic method
+	 * update a specify row
+	 * $record is an associate array [name => value]
+	 * $condition is a string
+	 * $order is an array [col_name, ASC | DESC]
+	 * $limite is a string or num
+	 */
+	public function update($record, $condition, $order, $rowcount){
+		if(!is_array($record)) return false;
+		$updateExp = "";
+		$this->_decorate($record); //decorate the value. add "".
+		while(list($name, $value)=each($record)){
+			$updateExp = ($updateExp!="") ? $updateExp.",".$name."=".$value : $updateExp.$name."=".$value;
+		}
+		$where_condition = $condition ? "WHERE $condition" : "";
+		$orderby = $order ? "ORDER BY $order[0] $order[1]" : "";
+		$limit = $rowcount ? "LIMIT $rowcount" : "";
+		$query = "UPDATE $this->db_table SET $updateExp $where_condition $orderby $limit";
+		$this->query($query);
+	}
+
+	/**
+	 * basic method
+	 */
+	public function delete($condition){
+		$where_condition = $condition ? "WHERE $condition" : "";
+		$query = "DELETE FROM $this->db_table $where_condition";
+		$this->query($query);
+	}
 }
 
 
@@ -135,3 +230,10 @@ $yangsql = new yangMysql(); //instantiation
 $yangsql->getCharset(); //test queryCharset()
 $yangsql->selectDb("ordermeal"); //
 $yangsql->showTables(); //
+$yangsql->selectTable("personnel");
+$yangsql->insert(["personnelId"=>1, "name"=>"测试", "engName"=>"test", "age"=>33, "department"=>1]); //
+$yangsql->simpleSelect(null,"personnelId=1",null,null);
+$yangsql->update(["name"=>"999", "engName"=>"nineninenine", "age"=>null, "department"=>null], "personnelId=1", null, null);
+$yangsql->simpleSelect(null,"personnelId=1",null,null);
+$yangsql->delete("personnelId=1");
+$yangsql->truncateTable("order160808"); //
